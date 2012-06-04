50bccf1c5376109d460586c37757995c99200ae9
David Sveningsson <ext@sidvind.com>
2012-06-04 13:30:30 +0200

    Update documentation.

--- datapack.h
+++ datapack.h
@@ -16,13 +16,24 @@ struct datapack_file_entry {
 };
 
 /**
- * If dir is non-null it allows overriding built-in files with local files from
- * dir. If null it disables overriding (default).
+ * Unpack a file using file entry directly.
+ * Allocated memory should be freed using free(3).
+ */
+int unpack(const struct datapack_file_entry* src, char** dst);
+
+/**
+ * Unpack a file using path.
+ * Allocated memory should be freed using free(3).
+ */
+int unpack_filename(const char* filename, char** dst);
+
+/**
+ * Allow user to override files by placing them in dir using the destination
+ * filename used when data was packed.
+ *
+ * If dir is NULL it disables overriding (default).
  */
 int unpack_override(const char* dir);
-
-int unpack(const struct datapack_file_entry* src, char** dst);
-int unpack_filename(const char* filename, char** dst);
 
 #ifdef _cplusplus
 }

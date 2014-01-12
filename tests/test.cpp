ce52e51461f27920c587d3d6299e8055a2a0bf2f
David Sveningsson <ext@sidvind.com>
2014-01-12 23:32:39 +0100

    fix tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,6 +4,8 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <cerrno>
 #include "data1.h"
 
 #include <cppunit/CompilerOutputter.h>
@@ -13,12 +15,13 @@
 
 class Test: public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(Test);
-  CPPUNIT_TEST( test_unpack );
+  CPPUNIT_TEST( test_unpack_inline );
   CPPUNIT_TEST( test_unpack_filename );
+  CPPUNIT_TEST( test_unpack_pack );
   CPPUNIT_TEST_SUITE_END();
 
 public:
-  void test_unpack(){
+  void test_unpack_inline(){
 	  char* tmp;
 	  unpack(&TEST_DATA_1, &tmp);
 	  CPPUNIT_ASSERT_EQUAL(std::string(tmp), std::string("test data\n"));
@@ -26,10 +29,39 @@ public:
   }
 
   void test_unpack_filename(){
+	  datapack_t handle = datapack_open(NULL);
+	  if ( !handle ){
+		  CPPUNIT_FAIL(std::string("unpack_open(..) failed: ") + strerror(errno));
+	  }
+
 	  char* tmp;
-	  unpack_filename("data2.txt", &tmp);
+	  int ret = unpack_filename(handle, "data2.txt", &tmp);
+	  if ( ret != 0 ){
+		  CPPUNIT_FAIL(std::string("unpack_filename(..) failed: ") + strerror(ret));
+	  }
+
 	  CPPUNIT_ASSERT_EQUAL(std::string(tmp), std::string("test data\n"));
 	  free(tmp);
+
+	  datapack_close(handle);
+  }
+
+  void test_unpack_pack(){
+	  datapack_t handle = datapack_open("tests/data2.pak");
+	  if ( !handle ){
+		  CPPUNIT_FAIL(std::string("unpack_open(..) failed: ") + strerror(errno));
+	  }
+
+	  char* tmp;
+	  int ret = unpack_filename(handle, "data3.txt", &tmp);
+	  if ( ret != 0 ){
+		  CPPUNIT_FAIL(std::string("unpack_filename(..) failed: ") + strerror(ret));
+	  }
+
+	  CPPUNIT_ASSERT_EQUAL(std::string(tmp), std::string("test data\n"));
+	  free(tmp);
+
+	  datapack_close(handle);
   }
 };
 
